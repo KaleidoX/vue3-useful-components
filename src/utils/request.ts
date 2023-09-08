c2be7661433cfdbad0d8e00a5665fd48d9ae51d8
xm <598308545@qq.com>
2023-09-08 14:31:21 +0800

    future: update axios by class

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,98 +1,149 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios'
-import errorCode from '@/utils/errorCode'
+// 作者：苏苏同学
+// 链接：https://juejin.cn/post/7113475007598034951
+// 来源：稀土掘金
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+import axios from 'axios'
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 
-// 创建axios实例
-const service: AxiosInstance = axios.create({
-  // axios中请求配置有baseURL选项，表示请求URL公共部分
-  // baseURL: '',
-  // 超时
-  // timeout: 3000,
-})
+type Result<T> = {
+  code: number
+  message: string
+  result: T
+}
 
-// service.interceptors.request.use(function (config: AxiosRequestConfig) {
-//   // Do something before request is sent
-//   return config;
-// }, function (error) {
-//   // Do something with request error
-//   return Promise.reject(error);
-// });
+// 导出Request类，可以用来自定义传递配置来创建实例
+export class Request {
+  // axios 实例
+  instance: AxiosInstance
+  // 基础配置，url和超时时间
+  baseConfig: AxiosRequestConfig = { baseURL: '/api', timeout: 60000 }
 
-// request拦截器
-// service.interceptors.request.use(
-//   (config) => {
-//     // 是否需要设置 token
-//     const isToken = (config.headers || {}).isToken === false;
-//     if (getToken() && !isToken) {
-//       config.headers["Authorization"] = "Bearer " + getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
-
-// let ConfirmLoginOut = null;
-
-// 响应拦截器
-service.interceptors.response.use(
-  (res: AxiosResponse) => {
-    // 未设置状态码则默认成功状态
-    const code = res.data?.code || 200
-    // 获取错误信息
-    const msg: string = errorCode[code] || res.data.msg || errorCode['default']
+  getMessageByCode(code: number, message = '') {
+    // 这里用来处理http常见错误，进行全局提示
+    let errorMessage = ''
     switch (code) {
-      case 401: {
-        // if (ConfirmLoginOut) {
-        //   return;
-        // }
-        // ConfirmLoginOut = MessageBox.confirm(
-        //   "登录状态已过期，您可以继续留在该页面，或者重新登录",
-        //   "系统提示",
-        //   {
-        //     confirmButtonText: "重新登录",
-        //     cancelButtonText: "取消",
-        //     type: "warning",
-        //   }
-        // )
-        //   .then(() => {
-        //     store.dispatch("LogOut").then(() => {
-        //       router.push("/index");
-        //     });
-        //   })
-        //   .catch(() => {})
-        //   .finally(() => {
-        //     ConfirmLoginOut = null;
-        //   });
-        return Promise.reject('令牌验证失败')
-      }
-      case 500: {
-        return Promise.reject(new Error(msg))
-      }
-      case 1:
-      case 200:
-      case 3000: {
-        return res.data
-      }
-
-      default: {
-        return Promise.reject(res.data)
-      }
+      case 400:
+        errorMessage = '请求错误(400)'
+        break
+      case 401:
+        errorMessage = '未授权，请重新登录(401)'
+        // 这里可以做清空storage并跳转到登录页的操作
+        break
+      case 403:
+        errorMessage = '拒绝访问(403)'
+        break
+      case 404:
+        errorMessage = '请求出错(404)'
+        break
+      case 408:
+        errorMessage = '请求超时(408)'
+        break
+      case 500:
+        errorMessage = '服务器错误(500)'
+        break
+      case 501:
+        errorMessage = '服务未实现(501)'
+        break
+      case 502:
+        errorMessage = '网络错误(502)'
+        break
+      case 503:
+        errorMessage = '服务不可用(503)'
+        break
+      case 504:
+        errorMessage = '网络超时(504)'
+        break
+      case 505:
+        errorMessage = 'HTTP版本不受支持(505)'
+        break
+      default:
+        errorMessage = `连接出错(${message})!`
     }
-  },
-  (error: AxiosResponse) => {
-    let { message } = error
-    if (!message) {
-      message = ''
-    } else if (message == 'Network Error') {
-      message = '后端接口连接异常'
-    } else if (message.includes('timeout')) {
-      message = '系统接口请求超时'
-    } else if (message.includes('Request failed with status code')) {
-      message = '系统接口' + message.substr(message.length - 3) + '异常'
-    }
-    return Promise.reject(error)
+    return errorMessage
   }
-)
 
-export default service
+  constructor(config: AxiosRequestConfig) {
+    // 使用axios.create创建axios实例
+    this.instance = axios.create(Object.assign(this.baseConfig, config))
+
+    this.instance.interceptors.request.use(
+      (config: AxiosRequestConfig) => {
+        // 一般会请求拦截里面加token，用于后端的验证
+        const token = localStorage.getItem('token') as string
+        if (token) {
+          config.headers!.Authorization = token
+        }
+
+        return config
+      },
+      (err: any) => {
+        // 请求错误，这里可以用全局提示框进行提示
+        return Promise.reject(err)
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (res: AxiosResponse) => {
+        // 直接返回res，当然你也可以只返回res.data
+        // 系统如果有自定义code也可以在这里处理
+        if (![1, 200].includes(res.data?.code)) {
+          const errorMessage = this.getMessageByCode(res.data?.code, res.data?.message)
+          console.error(errorMessage)
+        }
+        return {
+          status: res.data?.code ?? res.response.status,
+          data: res.data?.data ?? res.data,
+          message: res.data?.message ?? res.response.message
+        }
+      },
+      (err: any) => {
+        const errorMessage = this.getMessageByCode(err.response.status, err.response.message)
+        console.error(errorMessage)
+        // 这里错误消息可以使用全局弹框展示出来
+        // 比如element plus 可以使用 ElMessage
+        // ElMessage({
+        //   showClose: true,
+        //   message: `请检查网络或联系管理员！${errorMessage}`,
+        //   type: "error",
+        // });
+        // 这里是AxiosError类型，所以一般我们只reject我们需要的响应即可
+        return Promise.reject(err.response)
+      }
+    )
+  }
+
+  // 定义请求方法
+  public request(config: AxiosRequestConfig): Promise<AxiosResponse> {
+    return this.instance.request(config)
+  }
+
+  public get<T = any>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<Result<T>>> {
+    return this.instance.get(url, config)
+  }
+
+  public post<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<Result<T>>> {
+    return this.instance.post(url, data, config)
+  }
+
+  public put<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<Result<T>>> {
+    return this.instance.put(url, data, config)
+  }
+
+  public delete<T = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<Result<T>>> {
+    return this.instance.delete(url, config)
+  }
+}
+
+// 默认导出Request实例
+export default new Request({})
