f9d80a3dd9f688802d884e25673c332064c57b18
xm <598308545@qq.com>
2023-09-08 15:57:20 +0800

    future: add image upload components

---
+++ src/api/upload.ts
@@ -0,0 +1,15 @@
+import md5 from 'md5'
+
+/**
+ * 上传文件
+ * @param {File} file 文件
+ * @returns
+ */
+export const uploadImage = (file: File) => {
+  return new Promise((resolve, reject) => {
+    resolve({
+      name: file.name,
+      url: `/upload/image/${md5(file.name)}`
+    })
+  })
+}
---
+++ src/components/upload/UploadImageVant.vue
@@ -0,0 +1,76 @@
+<template>
+  <van-uploader
+    v-model="fileList"
+    accept="image/*"
+    :multiple="limit > 1"
+    :max-count="limit"
+    preview-image
+    deletable
+    image-fit="container"
+    result-type="file"
+    :after-read="handleRead"
+  />
+</template>
+
+<script>
+import { isString } from 'lodash-es'
+import { uploadImage } from '@/api/upload'
+
+export default {
+  name: 'UploadImageVant',
+  props: {
+    modelValue: [String, Array],
+    // 图片数量限制
+    limit: {
+      type: Number,
+      default: 5
+    },
+    // 大小限制(MB)
+    fileSize: {
+      type: Number,
+      default: 5
+    },
+    // 是否显示提示
+    isShowTip: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup(props, { emit }) {
+    const fileList = computed({
+      get: () => {
+        if (props.modelValue) {
+          const list = isString(props.modelValue) ? props.modelValue.split(',') : props.modelValue
+          return list.map((item) => {
+            if (isString(item)) {
+              item = { name: item, url: item }
+            }
+            return item
+          })
+        } else {
+          return []
+        }
+      },
+      set: (newValue) => {
+        emit('update:modelValue', newValue)
+        return newValue
+      }
+    })
+    const handleRead = (file) => {
+      file.status = 'uploading'
+      file.message = '上传中...'
+      uploadImage(file.file)
+        .then(({ url }) => {
+          file.status = 'done'
+          file.message = '上传完成'
+          file.url = url
+        })
+        .catch(() => {
+          file.status = 'failed'
+          file.message = '上传失败'
+        })
+    }
+    return { fileList, handleRead }
+  }
+}
+</script>
