92cb7083ab13c9d4c5aec6596ca42352e5bb0527
xm <598308545@qq.com>
2023-10-09 11:40:00 +0800

    future: add input checkbox components

---
+++ src/components/form/InputCheckbox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="h-6 w-6 flex items-center justify-center border rounded-md border-solid color-base-100"
+    :class="checked ? 'bg-success border-success' : 'bg-base-100 border-border'"
+  >
+    <i class="i-ep:select"></i>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'InputCheckbox',
+  props: {
+    checked: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
