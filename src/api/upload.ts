072060978ec2ba07d702f2aee07ac13311f2e241
xm <598308545@qq.com>
2024-01-05 10:32:51 +0800

    fix: fix get result from imgToBase64

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -6,11 +6,12 @@ import md5 from 'md5'
  * @returns
  */
 export const uploadImage = (file: File) => {
-  return new Promise((resolve) => {
+  return new Promise(async (resolve) => {
+    const url = await imgToBase64(file)
     resolve({
       name: file.name,
       // url: `/upload/image/${md5(file.name)}`
-      url: imgToBase64(file)
+      url: url
     })
   })
 }
