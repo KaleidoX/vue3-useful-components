2f567a4f07222e934972437d093ac358b14b4f9c
xm <598308545@qq.com>
2024-01-05 10:16:22 +0800

    fea: add form rich editor quill

---
+++ src/components/form/EditorQuill.vue
@@ -0,0 +1,364 @@
+<template>
+  <div class="editor" :class="{ 'editor-simple': simple, 'hidden-toolbar': hiddenToolbar }">
+    <quill-editor
+      ref="quillEditorRef"
+      theme="snow"
+      v-model:content="content"
+      contentType="html"
+      @textChange="(e) => $emit('update:modelValue', content)"
+      :modules="modules"
+      :options="options"
+      :toolbar="toolbar"
+      :style="styles"
+    />
+    <div class="text-right" v-if="maxLength">限制{{ maxLength }}个字</div>
+  </div>
+</template>
+
+<script setup>
+import { ElMessage } from 'element-plus'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { QuillEditor, Quill } from '@vueup/vue-quill'
+import ImageUploader from 'quill-image-uploader'
+import 'quill-image-uploader/dist/quill.imageUploader.min.css'
+import BlotFormatter from 'quill-blot-formatter'
+import Mention from 'quill-mention'
+import { uploadImage } from '@/api/upload.ts'
+import { formatUploadBase } from '@/utils/format.ts'
+
+defineOptions({
+  name: 'EditorQuill'
+})
+
+const quillEditorRef = ref()
+// console.log(Quill, Quill.register)
+// if (!Quill.imports['modules/ImageUploader']) {
+//   Quill.register('modules/imageUploader', ImageUploader)
+// }
+// if (!Quill.imports['modules/BlotFormatter']) {
+//   Quill.register('modules/blotFormatter', BlotFormatter)
+// }
+
+const getEditor = () => {
+  return quillEditorRef.value
+}
+const getQuill = () => {
+  return toRaw(quillEditorRef.value)?.getQuill()
+}
+const getModule = (moduleName) => {
+  return getQuill().getModule(moduleName)
+}
+
+const props = defineProps({
+  /* 编辑器的内容 */
+  modelValue: {
+    type: String
+  },
+  /* 高度 */
+  height: {
+    type: Number,
+    default: null
+  },
+  /* 最小高度 */
+  minHeight: {
+    type: Number,
+    default: 160
+  },
+  /* 只读 */
+  readOnly: {
+    type: Boolean,
+    default: false
+  },
+  /* 上传文件大小限制(MB) */
+  fileSize: {
+    type: Number,
+    default: 5
+  },
+  /* 类型（base64格式、url格式） */
+  type: {
+    type: String,
+    default: 'url'
+  },
+  maxLength: Number,
+  /* 占位提示信息 */
+  placeholder: {
+    type: String,
+    default: '请输入内容'
+  },
+  hiddenToolbar: {
+    type: Boolean,
+    default: false
+  },
+  simple: {
+    type: Boolean,
+    default: false
+  }
+})
+const modules = [
+  {
+    name: 'imageUploader',
+    module: ImageUploader,
+    options: {
+      upload: (file) => {
+        return new Promise((resolve, reject) => {
+          if (!handleBeforeUpload(file)) {
+            return reject('error')
+          }
+          uploadImage(file)
+            .then((res) => {
+              resolve(formatUploadBase(res.url))
+            })
+            .catch((err) => {
+              reject(err)
+            })
+        })
+      }
+    }
+  },
+  {
+    name: 'blotFormatter',
+    module: BlotFormatter,
+    options: {
+      /* options */
+    }
+  },
+  {
+    name: 'mention',
+    module: Mention,
+    options: {
+      mentionDenotationChars: ['@'],
+      spaceAfterInsert: false,
+      // eslint-disable-next-line no-unused-vars
+      source: function (searchTerm, renderList, mentionChar) {}
+    }
+  }
+]
+const toolbar = props.simple
+  ? [['image']]
+  : [
+      ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
+      ['blockquote', 'code-block'], // 引用  代码块
+      [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
+      [{ indent: '-1' }, { indent: '+1' }], // 缩进
+      [{ size: ['small', false, 'large', 'huge'] }], // 字体大小
+      [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
+      [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
+      [{ align: [] }], // 对齐方式
+      ['clean'], // 清除文本格式
+      ['link', 'image', 'video'] // 链接、图片、视频
+    ]
+const options = {
+  theme: 'snow',
+  bounds: document.body,
+  debug: 'error',
+  placeholder: props.placeholder,
+  readOnly: props.readOnly
+  // formats: ['bold', 'italic', 'mention']
+}
+
+const styles = computed(() => {
+  let style = {}
+  if (props.minHeight) {
+    style.minHeight = `${props.minHeight}px`
+  }
+  if (props.height) {
+    style.height = `${props.height}px`
+  }
+  return style
+})
+
+const content = ref('')
+watch(
+  () => props.modelValue,
+  (v) => {
+    if (!v) {
+      clearContent()
+    } else if (v !== content.value) {
+      content.value = v === undefined ? '<p></p>' : v
+    }
+  },
+  { immediate: true }
+)
+
+// 如果设置了上传地址则自定义图片上传事件
+onMounted(() => {
+  // if (props.type == 'url') {
+  //   const quill = getQuill()
+  //   const toolbar = quill.getModule('toolbar')
+  //   toolbar.addHandler('image', (value) => {
+  //     if (value) {
+  //       uploadRef.value.click()
+  //     } else {
+  //       quill.format('image', false)
+  //     }
+  //   })
+  // }
+})
+
+// 上传前校检格式和大小
+function handleBeforeUpload(file) {
+  const type = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg']
+  const isJPG = type.includes(file.type)
+  //检验文件格式
+  if (!isJPG) {
+    ElMessage.error({ message: '图片格式错误!' })
+    return false
+  }
+  // 校检文件大小
+  if (props.fileSize) {
+    const isLt = file.size / 1024 / 1024 < props.fileSize
+    if (!isLt) {
+      ElMessage.error({ message: `上传文件大小不能超过 ${props.fileSize} MB!` })
+      return false
+    }
+  }
+  return true
+}
+
+// 清空内容
+function clearContent() {
+  const editor = getEditor()
+  if (editor) {
+    editor.setContents('')
+    const quill = getQuill()
+    quill.setSelection(0, 0)
+  }
+}
+// 增加 mention
+function addMention(mention) {
+  const quill = getQuill()
+  const deltaMention = quill
+    .getModule('mention')
+    .insertItem({ denotationChar: '@', ...mention }, true)
+    .filter((item) => item.insert)
+  quill.setContents(deltaMention)
+  quill.setSelection(quill.getLength() + 1)
+}
+
+// 定义组件接口
+defineExpose({
+  getEditor,
+  getQuill,
+  getModule,
+  addMention,
+  clearContent
+})
+</script>
+
+<style lang="scss">
+.editor-img-uploader {
+  display: none;
+}
+.editor {
+  position: relative;
+  &.hidden-toolbar {
+    .ql-toolbar {
+      display: none;
+    }
+    .ql-container.ql-snow {
+      border: 1px solid #d1d5db !important;
+    }
+  }
+}
+.editor,
+.ql-toolbar {
+  white-space: pre-wrap !important;
+  line-height: normal !important;
+}
+.ql-container .ql-editor {
+  min-height: 160px;
+}
+.editor-simple {
+  .ql-toolbar {
+    height: 0;
+    padding: 0;
+    border: none;
+    .ql-image {
+      position: absolute;
+      right: 8px;
+      bottom: 8px;
+      z-index: 10;
+    }
+  }
+  .ql-container.ql-snow {
+    border: 1px solid #d1d5db !important;
+  }
+}
+.quill-img {
+  display: none;
+}
+.ql-snow {
+  .ql-tooltip {
+    &[data-mode='link']::before {
+      content: '请输入链接地址:';
+    }
+    &.ql-editing a.ql-action::after {
+      border-right: 0;
+      content: '保存';
+      padding-right: 0;
+    }
+    &[data-mode='video']::before {
+      content: '请输入视频地址:';
+    }
+  }
+  .ql-picker {
+    &.ql-size {
+      .ql-picker-label,
+      .ql-picker-item {
+        &::before {
+          content: '14px';
+        }
+        &[data-value='small']::before {
+          content: '10px';
+        }
+        &[data-value='large']::before {
+          content: '18px';
+        }
+        &[data-value='huge']::before {
+          content: '32px';
+        }
+      }
+    }
+    &.ql-header {
+      .ql-picker-label,
+      .ql-picker-item {
+        &::before {
+          content: '文本';
+        }
+        &[data-value='1']::before {
+          content: '标题1';
+        }
+        &[data-value='2']::before {
+          content: '标题2';
+        }
+        &[data-value='3']::before {
+          content: '标题3';
+        }
+        &[data-value='4']::before {
+          content: '标题4';
+        }
+        &[data-value='5']::before {
+          content: '标题5';
+        }
+        &[data-value='6']::before {
+          content: '标题6';
+        }
+      }
+    }
+    &.ql-font {
+      .ql-picker-label,
+      .ql-picker-item {
+        &::before {
+          content: '标准字体';
+        }
+        &[data-value='serif']::before {
+          content: '衬线字体';
+        }
+        &[data-value='monospace']::before {
+          content: '等宽字体';
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/form/EditorQuillView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-5">
+    <EditorQuill v-model="content1" />
+  </div>
+  <div class="p-5">
+    <EditorQuill v-model="content2" simple />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'EditorQuillView'
+})
+const content1 = ref('')
+const content2 = ref('')
+</script>
