1d184ab5dbcd68d07a4f8393030de3159b2821ec
xm <598308545@qq.com>
2024-04-03 17:03:22 +0800

    feat: upgrade video test url

--- src/views/play/videoTestList.ts
+++ src/views/play/videoTestList.ts
@@ -1,4 +1,10 @@
 export const testM3u8 = 'http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8'
+export const testM3u8_2 =
+  '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/hls/xgplayer-demo.m3u8'
+
 export const testMp4 = 'http://www.w3school.com.cn/i/movie.mp4'
+export const testMp4_2 =
+  '//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4'
+
 export const testFlv =
-  'https://sf1-hscdn-tos.pstatp.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-720p.flv'
+  'https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/flv/xgplayer-demo-360p.flv'
