b5c2a46f98d5ebc1e1ec3d43cdbd4b1de6a6ab6c
xm <598308545@qq.com>
2025-10-31 11:35:23 +0800

    feat(EditorQuillView): 给quill编辑器视图添加初始数据

--- src/views/form/EditorQuillView.vue
+++ src/views/form/EditorQuillView.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="p-5">
-    <EditorQuill v-model="content1" />
-  </div>
-  <div class="p-5">
-    <EditorQuill v-model="content2" simple />
-  </div>
+  <section class="grid grid-cols-1 gap-10 p-10 lg:grid-cols-2">
+    <EditorQuill v-model="content1" :height="editorHeight" />
+    <EditorQuill v-model="content2" :height="editorHeight" simple />
+  </section>
 </template>
 
 <script lang="ts" setup>
+import TestContent from '@/views/form/assets/test.html?raw'
+
 defineOptions({
   name: 'EditorQuillView'
 })
-const content1 = ref('')
-const content2 = ref('')
+const content1 = ref(TestContent)
+const content2 = ref(TestContent)
+
+const { width, height } = useWindowSize()
+
+const safeAreaInsetTop = parseInt(
+  document.documentElement.style.getPropertyValue('--safe-area-inset-top') || '0px'
+)
+const safeAreaInsetBottom = parseInt(
+  document.documentElement.style.getPropertyValue('--safe-area-inset-bottom') || '0px'
+)
+
+const editorHeight = computed(() => {
+  const fullHeight = height.value - (46 + 4 * 10 * 2 + safeAreaInsetTop + safeAreaInsetBottom)
+  return width.value > 1024 ? fullHeight : Math.ceil((fullHeight - 4 * 10) / 2)
+})
 </script>
