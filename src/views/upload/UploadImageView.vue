f3414c3601bc41063c253d14d576070b7c4d6516
xm <598308545@qq.com>
2026-03-18 20:54:00 +0800

    fix(router): stabilize preview view transitions

--- src/views/upload/UploadImageView.vue
+++ src/views/upload/UploadImageView.vue
@@ -1,8 +1,6 @@
 <template>
-  <section class="mt-5 px-4">
+  <section class="grid gap-5">
     <UploadImageVant v-model="uploadListVant" />
-  </section>
-  <section class="mt-5 px-4">
     <UploadImageElement v-model="uploadListElement" />
   </section>
 </template>
