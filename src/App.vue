97b9ace479ff87de48527274501bfa264941f6f0
xm <598308545@qq.com>
2026-04-29 17:06:24 +0800

    feat(App): 默认提供更大的可视区域宽度

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     />
   </div> -->
   <el-config-provider :locale="localeElement">
-    <div class="mx-auto max-w-[1600px] px-4 py-6 lg:p-8 sm:px-6">
+    <div class="mx-auto max-w-[90vw] px-4 py-6 lg:p-8 sm:px-6">
       <div class="grid gap-6 lg:grid-cols-[320px_minmax(0,1fr)]">
         <LayoutNav class="lg:sticky lg:top-8 lg:max-h-[calc(100vh-4rem)]" />
         <LayoutMain>
